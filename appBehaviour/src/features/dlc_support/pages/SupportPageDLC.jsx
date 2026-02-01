a998de720ac6df2f91dfabde275025a981e1c1b0
GuilhermeCF <guicostaferreira.2003@gmail.com>
2026-02-01 16:38:11 +0100

    refractor: moved the SupportTitle to a shared UI component under the name SectionTitle

--- appBehaviour/src/features/dlc_support/pages/SupportPageDLC.jsx
+++ appBehaviour/src/features/dlc_support/pages/SupportPageDLC.jsx
@@ -1,13 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import Button from "../../../shared/ui/Button/Button"
-import SupportTitle from "../components/SupportTitle/SupportTitle";
+import SectionTitle from "../../../shared/ui/SectionTitle/SectionTitle";
 
 function SupportPageDLC() {
   const navigate = useNavigate();
 
   return (
     <div>
-      <SupportTitle>DLC Support</SupportTitle>
+      <SectionTitle>DLC Support</SectionTitle>
       <Button className="container" onClick={() => navigate("/")}>Go Back</Button>
     </div>
   )

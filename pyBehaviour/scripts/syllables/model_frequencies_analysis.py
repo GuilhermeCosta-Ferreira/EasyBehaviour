4f1e92bf90a5f9a5167dd7d5cfb8676d4f40b0a2
GuilhermeCF <guicostaferreira.2003@gmail.com>
2026-02-13 10:26:29 +0100

    refractor: refrafctor visualization for average densities

--- pyBehaviour/scripts/syllables/model_frequencies_analysis.py
+++ pyBehaviour/scripts/syllables/model_frequencies_analysis.py
@@ -6,7 +6,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 from pathlib import Path
 from pybehaviour.syllable_analysis import (
-    get_syllable_density
+    get_syllable_density,
+    get_average_syllables
 )
 
 
@@ -15,10 +16,15 @@ from pybehaviour.syllable_analysis import (
 # 1. Section: INPUTS
 # ================================================================
 RESULTS_FOLDER: Path = Path("../data/syllables/results")
+"""
 FAVORITE_MODELS: list[str] =  [
     "2026_02_11-13_18_50",
     "2026_02_11-13_52_58",
     "2026_02_11-14_31_04"
+]
+"""
+FAVORITE_MODELS: list[str] =  [
+    "model_50_all"
 ]
 
 
@@ -26,33 +32,21 @@ FAVORITE_MODELS: list[str] =  [
 # ================================================================
 # 2. Section: FUNCTIONS
 # ================================================================
-def inspect(folder: Path, limit: tuple):
-    syllable_store = get_syllable_density(folder)
+def inspect_model_denisty_distribution(folder: Path, limit: tuple):
+    syllables = get_syllable_density(folder)
+    average_syllables = get_average_syllables(syllables)
 
-
-    average_bl_counts = np.mean(
-        a=np.vstack([dp.counts for dp in syllable_store if dp.timepoint == "BL"]),
-        axis=0
-    )
-    average_w1_counts = np.mean(
-        a=np.vstack([dp.counts for dp in syllable_store if dp.timepoint == "Post_W1"]),
-        axis=0
-    )
-    average_w4_counts = np.mean(
-        a=np.vstack([dp.counts for dp in syllable_store if dp.timepoint == "Post_W4"]),
-        axis=0
-    )
-    average_w8_counts = np.mean(
-        a=np.vstack([dp.counts for dp in syllable_store if dp.timepoint == "Post_W8"]),
-        axis=0
-    )
     centers = np.arange(0, 100 + 1)  # integer centers
 
     plt.figure()
-    plt.plot(centers, average_bl_counts, marker="o", label="BL")
-    plt.plot(centers, average_w1_counts, marker="o", label="W1")
-    plt.plot(centers, average_w4_counts, marker="o", label="W4")
-    plt.plot(centers, average_w8_counts, marker="o", label="W8")
+    for syl in average_syllables:
+        plt.plot(
+            centers,
+            syl.counts,
+            marker="o",
+            label=f"#{syl.group}_{syl.timepoint}"
+        )
+
     plt.xlim((limit[0] - 0.5, limit[1] - 0.5))
     plt.xticks(np.arange(limit[0], limit[1]))
     plt.legend()
@@ -70,7 +64,7 @@ if __name__ == '__main__':
 
 
     for model in models:
-        inspect(
+        inspect_model_denisty_distribution(
             folder = model,
             limit = (0, 21))
     plt.show()

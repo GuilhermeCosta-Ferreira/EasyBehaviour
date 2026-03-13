1774b4b52d98cb25e7827257e0a53da689bf5a9f
GuilhermeCF <guicostaferreira.2003@gmail.com>
2026-03-13 14:29:39 +0100

    fix: added control for ploting test

--- pyBehaviour/scripts/reaching/multi_group_distance.py
+++ pyBehaviour/scripts/reaching/multi_group_distance.py
@@ -15,7 +15,8 @@ from pybehaviour.reaching import scrap_folder
 # ================================================================
 DLC_FOLDER: Path = Path("../data/dlc")
 COMPARING_GROUP_FOLDER: Path = DLC_FOLDER / "71_reaching"
-CONTROL_GROUP_FOLDER: Path = DLC_FOLDER / "46_reaching"
+#CONTROL_GROUP_FOLDER: Path = DLC_FOLDER / "46_reaching"
+CONTROL_GROUP_FOLDER: Path = DLC_FOLDER / "71_reaching"
 
 
 
@@ -29,10 +30,8 @@ CONTROL_GROUP_FOLDER: Path = DLC_FOLDER / "46_reaching"
 # 3. Section: MAIN
 # ================================================================
 if __name__ == '__main__':
-    csv_files_names = np.array([filename for filename in os.listdir(COMPARING_GROUP_FOLDER) if filename.endswith('.csv')])
-    timepoint_json = np.array([filename for filename in os.listdir(COMPARING_GROUP_FOLDER) if filename.endswith('.json')])
-
     study_group = scrap_folder(COMPARING_GROUP_FOLDER)
+    control_group = scrap_folder(CONTROL_GROUP_FOLDER)
 
     print(study_group.min_distances_per_tp)
     print(study_group.mean_min_distance_per_tp)

de8119c13151c7ba6e2c04e91f1ff46a11887041
GuilhermeCF <guicostaferreira.2003@gmail.com>
2026-03-19 18:26:14 +0100

    feat: added script for file excel file construction

--- pyBehaviour/scripts/reaching/get_file_csv.py
+++ pyBehaviour/scripts/reaching/get_file_csv.py
@@ -1,18 +1,19 @@
 # ================================================================
 # 0. Section: IMPORTS
 # ================================================================
+import pandas as pd
+
+from pathlib import Path
+
+from pybehaviour.reaching.io import get_labels_path
 
 
 
 # ================================================================
 # 1. Section: INPUTS
 # ================================================================
-
-
-
-# ================================================================
-# 2. Section: FUNCTIONS
-# ================================================================
+DLC_FOLDER: Path = Path("../data/dlc")
+GROUP_FOLDER: Path = DLC_FOLDER / "71_reaching"
 
 
 
@@ -20,4 +21,19 @@
 # 3. Section: MAIN
 # ================================================================
 if __name__ == '__main__':
-    pass
+    # 1. Get the sorted csv files
+    csv_files = get_labels_path(GROUP_FOLDER / "raw_csv")
+    csv_files = [file.name.split("DLC")[0] for file in csv_files]
+
+    # 2. Generate the dataframe
+    df = pd.DataFrame(
+        {
+            "name": csv_files,
+            "state": None
+        }
+    )
+
+    # 3. Save it as excel in the group folder
+    file_path = GROUP_FOLDER / "file_list_label.xlsx"
+    df.to_excel(file_path, index=False)
+    print(f"Saved at {file_path}")
